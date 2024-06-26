8c5a411cd4c0e06f5639479e3ba165549a4e7071
Chris Penny <chris.penny@gmail.com>
2024-06-26 12:35:03 +1200

    Update search service requests for Bifrost endpoints

---
+++ src/Service/Requests/ClickPost.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace SilverStripe\DiscovererBifrost\Service\Requests;
+
+use Elastic\EnterpriseSearch\AppSearch\Request\LogClickthrough as AppSearchLogClickthrough;
+use Elastic\EnterpriseSearch\AppSearch\Schema\ClickParams;
+
+class ClickPost extends AppSearchLogClickthrough
+{
+
+    public function __construct(string $engineName, ?ClickParams $click_params = null)
+    {
+        parent::__construct($engineName, $click_params);
+
+        $this->path = sprintf('/api/v1/%s/click/', $engineName);
+    }
+
+}
---
+++ src/Service/Requests/Search.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace SilverStripe\DiscovererBifrost\Service\Requests;
+
+use Elastic\EnterpriseSearch\AppSearch\Request\Search as AppSearchSearch;
+use Elastic\EnterpriseSearch\AppSearch\Schema\SearchRequestParams;
+
+class Search extends AppSearchSearch
+{
+
+    public function __construct(string $engineName, ?SearchRequestParams $search_request_params = null)
+    {
+        parent::__construct($engineName, $search_request_params);
+
+        $this->path = sprintf('/api/v1/%s/search/', $engineName);
+    }
+
+}
