89ee60e9c2fb0ca6eb2bae55b8052b1100f7e49b
Chris Penny <chris.penny@gmail.com>
2024-11-06 14:10:29 +1300

    Discoverer v2 support. Spelling suggestions feature (#7)

---
+++ src/Processors/SuggestionParamsProcessor.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace SilverStripe\DiscovererBifrost\Processors;
+
+use SilverStripe\Core\Injector\Injectable;
+use SilverStripe\Discoverer\Query\Suggestion;
+use SilverStripe\DiscovererBifrost\Service\Requests\Params\SuggestionParams;
+
+class SuggestionParamsProcessor
+{
+
+    use Injectable;
+
+    public function getQueryParams(Suggestion $suggestion): SuggestionParams
+    {
+        $suggestionParams = SuggestionParams::create();
+        $suggestionParams->query = $suggestion->getQueryString();
+        $suggestionParams->formatted = $suggestion->isFormatted();
+
+        $limit = $suggestion->getLimit();
+        $fields = $suggestion->getFields();
+
+        if ($limit) {
+            $suggestionParams->size = $limit;
+        }
+
+        if ($fields) {
+            $suggestionParams->fields = $fields;
+        }
+
+        return $suggestionParams;
+    }
+
+}
