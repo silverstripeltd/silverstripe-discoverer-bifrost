acc7a84c88f200b4efef31642d79f61e2ad6d60d
Chris Penny <chris.penny@gmail.com>
2025-05-22 10:12:44 +1200

    Add info logging for deprecation

--- src/Service/Adaptors/ProcessAnalyticsAdaptor.php
+++ src/Service/Adaptors/ProcessAnalyticsAdaptor.php
@@ -2,15 +2,33 @@
 
 namespace SilverStripe\DiscovererBifrost\Service\Adaptors;
 
+use Psr\Log\LoggerInterface;
 use SilverStripe\Discoverer\Analytics\AnalyticsData;
 use SilverStripe\Discoverer\Service\Interfaces\ProcessAnalyticsAdaptor as ProcessAnalyticsAdaptorInterface;
 
+/**
+ * @deprecated Removed in version 3.0. We recommend that you remove SEARCH_ANALYTICS_ENABLED
+ */
 class ProcessAnalyticsAdaptor implements ProcessAnalyticsAdaptorInterface
 {
 
+    private ?LoggerInterface $logger = null;
+
+    private static array $dependencies = [
+        'logger' => '%$' . LoggerInterface::class . '.errorhandler',
+    ];
+
+    public function setLogger(?LoggerInterface $logger): void
+    {
+        $this->logger = $logger;
+    }
+
     public function process(AnalyticsData $analyticsData): void
     {
-        // Silently do nothing. Silverstripe Search does not support analytics
+        $this->logger->info(
+            'SilverStripe\DiscovererBifrost\Service\Adaptors\ProcessAnalyticsAdaptor is deprecated and will be removed'
+            . ' in version 3.0. We recommend that you remove SEARCH_ANALYTICS_ENABLED'
+        );
     }
 
 }
